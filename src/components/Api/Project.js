47f163f188bea10dceead81a0cbeaab2d86b78e5
Your Name <you@example.com>
2024-11-13 21:43:41 +0530

    Added

--- src/components/Api/Project.js
+++ src/components/Api/Project.js
@@ -13,17 +13,15 @@ export const Projects = [
         title: 'Todo App',
         image: require('../../Assets/project/Todo.png'), // Ensure this path is correct
         details: 'A simple Todo App for managing tasks, featuring add and delete functionalities with a clean user interface.',
-        languages: ['HTML', 'CSS', 'JavaScript'],
         code: '#',
         liveLink: 'https://tododaliy.vercel.app/', // Updated live link
     },
-    // {
-    //     id: 3,
-    //     title: 'Blog Platform',
-    //     image: require('../../Assets/project/Todo.png'), // Add your image path here
-    //     details: 'A blogging platform where users can create, edit, and delete blog posts.',
-    //     languages: ['React', 'Django', 'PostgreSQL'],
-    //     code: '#',
-    //     liveLink: 'https://yourblogplatform.com',
-    // },
+    {
+        id: 3,
+        title: 'Restaurant-Demo',
+        image: require('../../Assets/project/3.png'), // Add your image path here
+        details: 'A blogging platform where users can create, edit, and delete blog posts.',
+        code: '#',
+        liveLink: 'https://vk-restaurant.vercel.app/',
+    },
 ];
