16b5c7296181208da028ccfa818d2bea720fa22c
Your Name <you@example.com>
2025-10-31 21:59:05 +0530

    update  code

--- src/components/Api/Project.js
+++ src/components/Api/Project.js
@@ -2,9 +2,9 @@ export const Projects = [
     {
         id: 1,
         title: 'Daily Blog',
-        image: require('../../Assets/project/blog.png'), 
+        image: require('../../Assets/project/blog.png'),
         details: 'A blogging platform built with Django where users can create, edit, and delete blog posts. It features user authentication, post management, and a responsive UI.',
-        languages: ['HTML', 'CSS', 'Python','Django','SQLite'],
+        languages: ['HTML', 'CSS', 'Python', 'Django', 'SQLite'],
         code: '#',
         liveLink: 'https://django-blog-wzf3.onrender.com',
     },
@@ -13,14 +13,16 @@ export const Projects = [
         title: 'React To-Do App',
         image: require('../../Assets/project/Todo.png'), // Ensure this path is correct
         details: 'A modern To-Do web app built with React. It allows users to add, delete, and manage daily tasks with a clean and responsive UI.',
+        languages: ['HTML', 'Tailwind CSS', 'JavaScript', 'ReactJS'],
         code: '#',
-        liveLink: 'https://react-todo-app-rho-eight.vercel.app/', 
+        liveLink: 'https://react-todo-app-rho-eight.vercel.app/',
     },
     {
         id: 3,
         title: 'Raj Electrical Services',
         image: require('../../Assets/project/raj.png'), // Add your image path here
         details: 'A service management web app for Raj Electricals to handle customer service requests, installations, and project bookings.',
+        languages: ['Node.js', 'Express.js', 'MongoDB', 'JavaScript', 'React.js', 'Tailwind CSS', 'Twilio'],
         code: '#',
         liveLink: 'https://rajelectricalservices.vercel.app/',
     },
@@ -29,6 +31,8 @@ export const Projects = [
         title: 'GuardianNet ',
         image: require('../../Assets/project/guardiannet.png'), // Add your image path here
         details: 'A MERN-based smart safety and alert system that helps users manage security and emergency notifications efficiently.',
+        languages: ['Node.js', 'Express.js', 'MongoDB', 'JavaScript', 'React.js', 'Tailwind CSS', 'Razorpay', ],
+
         code: '#',
         liveLink: 'https://guardiannet.vercel.app/',
     },
